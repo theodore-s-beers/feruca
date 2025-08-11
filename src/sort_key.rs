9674bdf0225006b2e9106e0c64bc21186db5afbf
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2025-08-11 13:49:47 -0400

    Simplify quaternary iterator

--- src/sort_key.rs
+++ src/sort_key.rs
@@ -149,14 +149,12 @@ fn compare_quaternary(a_cea: &[u32], b_cea: &[u32]) -> Option<Ordering> {
     let mut a_filter = a_cea
         .iter()
         .take_while(|x| **x < u32::MAX)
-        .filter(|w| variability(**w) || tertiary(**w) != 0)
         .map(|w| primary(*w))
         .filter(|q| *q != 0);
 
     let mut b_filter = b_cea
         .iter()
         .take_while(|x| **x < u32::MAX)
-        .filter(|w| variability(**w) || tertiary(**w) != 0)
         .map(|w| primary(*w))
         .filter(|q| *q != 0);
 
